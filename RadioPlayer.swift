804af3cdf2b12e7268c2c2bad4eb6b20b6b03090
Shapeotron <davidoleary1@yahoo.co.uk>
2015-12-05 15:33:25 +0000

    Final changes, code commented, tidied up EZAudio remnants

--- RadioPlayer.swift
+++ RadioPlayer.swift
@@ -6,24 +6,36 @@
 //  Copyright © 2015 Shapes. All rights reserved.
 //
 
+//Import of libraries
 import Foundation
 import AVFoundation
 
+//Class builder
 class RadioPlayer {
+    
+    //Creating an instance of the RadioPlayer. Access from viewcontroller
     static let sharedInstance = RadioPlayer()
+    
+    //Setting the default radio stream
     var player = AVPlayer(URL: NSURL(string: "http://s22.myradiostream.com:9368/")!)
+    
+    //Setting the player to not play when loaded
     var isPlaying = false
     
+    //Play the stream when function called
     func play() {
         player.play()
         isPlaying = true
     }
     
+    //Pause the stream when function called
     func pause() {
         player.pause()
         isPlaying = false
     }
     
+    //Toggle for checking whether isPlaying is currently true or false 
+    //and then pausing or playing based on status
     func toggle() {
         if isPlaying == true {
             pause()
@@ -32,6 +44,7 @@ class RadioPlayer {
         }
     }
     
+    //Return of isPlaying status
     func currentlyPlaying() -> Bool {
         return isPlaying
     }
--- ViewController.swift
+++ ViewController.swift
@@ -6,46 +6,58 @@
 //  Copyright © 2015 Shapes. All rights reserved.
 //
 
+//Imported libraries
+
 import UIKit
 import AVFoundation
 import MediaPlayer
 
+//Class declaration and extensions
+class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
-class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource, EZMicrophoneDelegate {
-    
+    //Connecting sections of the main storyboard to the code
     @IBOutlet weak var trackPicker: UIPickerView!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var bgImage: UIImageView!
 
+    //Variables being declared
     var isPlaying = false
     var error = "Error"
     var picker1Options = [String]()
     
+    //ViewDidLoad method.
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
+        //Background mode for the app
         do{
         try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
         }catch{
             print(error)
         }
         
+        //Importing the stations from the plist
         let path = NSBundle.mainBundle().pathForResource("stations", ofType: "plist")
         let dict = NSDictionary(contentsOfFile: path!)
         
+        //Adding them to the picker1Option array
         picker1Options = dict!.objectForKey("stations") as! [String]
         
+        //Setting the default load image
         bgImage!.image = UIImage(named: "newviberadio.png")!
         self.trackPicker.delegate = self
         self.trackPicker.dataSource = self
     }
     
+    //Calling a func for button presses to ultimately return isPlaying status
     @IBAction func buttonPressed(sender: AnyObject) {
         
         toggle()
     }
     
+    //..which passes commands back to the RadioPlayer swift file to play/pause audio 
+    //depending on what is currently active
     func toggle() {
         
         if RadioPlayer.sharedInstance.currentlyPlaying() {
@@ -55,18 +67,21 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
         }
     }
     
+    //Playing audio and changing the image
     func playRadio() {
         
         RadioPlayer.sharedInstance.play()
         playButton.setImage(UIImage(named: "pause.png") , forState: UIControlState.Normal)
     }
     
+    //Pausing radio and again changing the image
     func pauseRadio() {
         
         RadioPlayer.sharedInstance.pause()
         playButton.setImage(UIImage(named: "play.png") , forState: UIControlState.Normal)
     }
     
+    //Number of components in the pickerview
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         
         return 1
@@ -86,12 +101,14 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
             return "\(picker1Options[row])"
     }
     
+    //Chaging the colour of the pickerview rows to black. (Had previously used an inversion of black and white.
     func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        
         let attributedString = NSAttributedString(string: "\(picker1Options[row])", attributes: [NSForegroundColorAttributeName : UIColor.blackColor()])
         return attributedString
     }
     
-    // Capture the picker view selection
+    // Capture the picker view selection, set the audio stream and image accordingly
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 
             
@@ -125,7 +142,6 @@ class ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate
                 RadioPlayer.sharedInstance.player = AVPlayer(URL: NSURL(string: "http://s22.myradiostream.com:9368/")!)
                 bgImage!.image = UIImage(named: "newviberadio.png")!
             }
-        print("\(picker1Options[row])")
     }
     
     override func didReceiveMemoryWarning() {
